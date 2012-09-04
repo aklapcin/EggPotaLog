aa3f0087437ab9d0070a4ea86286adf34885fbc1
Aleksandra Klapcinska <aleksandra.klapcinska@gmail.com>
2012-09-05 01:50:55 +0200

    Text prop indexed

--- blogApp/posts/models.py
+++ blogApp/posts/models.py
@@ -7,7 +7,7 @@ class Post(db.Model):
     title = db.StringProperty()
     intro = db.TextProperty()
     slug = db.StringProperty()
-    content = db.TextProperty(indexed=True)
+    content = db.TextProperty()
     last_edited = db.DateTimeProperty(auto_now=True)
     date_created = db.DateTimeProperty(auto_now_add=True)
     user = db.ReferenceProperty(User)
